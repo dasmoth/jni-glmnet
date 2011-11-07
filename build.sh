a4841b177a15e07c07c13097a6bcb19a638a309e
Thomas Down <thomas.a.down@googlemail.com>
2011-11-07 22:20:12 +0000

    Static-link libgfortran et al. on OSX builds.

--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ export CLASSPATH=glmnet.jar
 javah -jni glmnet.GLMNet
 gcc -c -I/System/Library/Frameworks/JavaVM.framework/Headers glmnet.c
 gcc -c newGLMnet_f.f90  -m64 -fdefault-real-8 -ffixed-form -fPIC
-gcc -dynamiclib -o libglmnet.jnilib -lgfortran -lm -lquadmath glmnet.o newGLMnet_f.o
+# gcc -dynamiclib -o libglmnet.jnilib -lgfortran -lm -lquadmath glmnet.o newGLMnet_f.o
+gcc -dynamiclib -o libglmnet.jnilib glmnet.o newGLMnet_f.o -lm -static-libgcc /usr/local/lib/libgfortran.a /usr/local/lib/libquadmath.a
