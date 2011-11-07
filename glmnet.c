3eb91df30873af9a19805469e599724e61d1ece9
Thomas Down <thomas.a.down@googlemail.com>
2011-11-07 21:36:32 +0000

    Initial wrapper for splognet.

--- glmnet.c
+++ glmnet.c
@@ -81,7 +81,7 @@ JNIEXPORT jint JNICALL Java_glmnet_GLMNet_spelnet
 	   &covUpdating, 
 	   &alpha,
 	   &yl,
-	   &xpl,        /* why -1? */
+	   &xpl,        
 	   xxv,
 	   xpv,
 	   xiv,
@@ -124,6 +124,128 @@ JNIEXPORT jint JNICALL Java_glmnet_GLMNet_spelnet
   (*env)->ReleaseIntArrayElements(env, outCoeffPtrs, coeffPtrsV, 0);
   (*env)->ReleaseIntArrayElements(env, outCoeffCnts, coeffCntsV, 0);
   (*env)->ReleaseDoubleArrayElements(env, outRsq, rSquaredsV, 0);
+  (*env)->ReleaseDoubleArrayElements(env, outLambdas, lambdasUsedV, 0);
+  (*env)->ReleaseIntArrayElements(env, outNumPasses, numPassesV, 0);
+  
+  return error;
+}
+
+
+
+extern void splognet_(
+            const double* alpha,
+            const int* num_observations,
+            const int* num_predictors,
+
+	    const int* num_classes,
+
+            const double* vals,         // sparse matrix
+            const int* outer_pointers,  // sparse matrix
+            const int* inner_indices,   // sparse matrix
+
+            double* observations,              // vector, overwritten
+            double* offsets,                   // vector, overwritten
+            const int* flags,                  // vector
+            const double* predictor_penalties, // vector
+            const int* max_final_features,
+            const int* max_path_features,
+            const int* num_lambdas,
+            const double* lambda_min_ratio,
+            const double* user_defined_lambdas, // vector
+            const double* convergence_threshold,
+            const int* standardize_flag, // standardize observations
+            const int* maxit,           // maximum iterations
+	    const int* kopt,            // options: 0=Newton-Raphson, 1=modified
+            // Outputs
+            int* num_fits,              // actual number of lambdas used 
+            double* intercepts,         // vector of intercepts 
+            double* coeffs,             // compressed matrix values
+            int* coeffs_ptrs,           // compressed matrix ptrs
+            int* coeff_counts,          // number non-zero per sol
+            double* dev0,               // null deviance
+	    double* fdev,               // vector, fraction of deviance.
+            double* lambdas_used,       // actual lambda values vector
+            int* num_passes,            // total iterations
+            int* error_flag             // errors
+           );
+
+
+JNIEXPORT jint JNICALL Java_glmnet_GLMNet_splognet
+  (JNIEnv *env, jobject thiz, jdouble alpha, jint nc, jdoubleArray y, jdoubleArray offsets, jdoubleArray xx, jintArray xi, jintArray xp, jintArray mFlags, jdoubleArray penalties, jint maxFinal, jint maxPath, jint numLambdas, jdouble lambdaMinRatio, jdoubleArray userLambdas, jdouble convThreshold, jint standardize, jint maxit, jint kopt, jintArray outNumFits, jdoubleArray outIntercepts, jdoubleArray outCoeffs, jintArray outCoeffPtrs, jintArray outCoeffCnts, jdoubleArray outDev0, jdoubleArray outFdev, jdoubleArray outLambdas, jintArray outNumPasses)
+{
+  jsize yl = (*env)->GetArrayLength(env, y);
+  jsize xpl = (*env)->GetArrayLength(env, xp) - 1; 
+  jdouble *yv = (*env)->GetDoubleArrayElements(env, y, NULL);
+  jdouble *ov = (*env)->GetDoubleArrayElements(env, offsets, NULL);
+  jdouble *xxv = (*env)->GetDoubleArrayElements(env, xx, NULL);
+  jint *xiv = (*env)->GetIntArrayElements(env, xi, NULL);
+  jint *xpv = (*env)->GetIntArrayElements(env, xp, NULL);
+  jint *mFlagsV = (*env)->GetIntArrayElements(env, mFlags, NULL);
+  jdouble *penaltiesV = (*env)->GetDoubleArrayElements(env, penalties, NULL);
+  jdouble *userLambdasV = (*env)->GetDoubleArrayElements(env, userLambdas, NULL);
+  jint *numFitsV = (*env)->GetIntArrayElements(env, outNumFits, NULL);
+  jdouble *interceptsV = (*env)->GetDoubleArrayElements(env, outIntercepts, NULL);
+  jdouble *coeffsV = (*env)->GetDoubleArrayElements(env, outCoeffs, NULL);
+  jint* coeffPtrsV = (*env)->GetIntArrayElements(env, outCoeffPtrs, NULL);
+  jint* coeffCntsV = (*env)->GetIntArrayElements(env, outCoeffCnts, NULL);
+  jdouble* dev0V = (*env)->GetDoubleArrayElements(env, outDev0, NULL);
+  jdouble* fdevV = (*env)->GetDoubleArrayElements(env, outFdev, NULL);
+  jdouble* lambdasUsedV = (*env)->GetDoubleArrayElements(env, outLambdas, NULL);
+  jint* numPassesV = (*env)->GetIntArrayElements(env, outNumPasses, NULL);
+  jint error = 0;
+
+  fprintf(stderr, "Calling splognet %d:%d\n", yl, xpl);
+  
+  splognet_(
+	   &alpha,
+	   &yl,
+	   &xpl,
+	   &nc,
+	   xxv,
+	   xpv,
+	   xiv,
+	   yv,
+	   ov,
+	   mFlagsV,
+	   penaltiesV,
+	   &maxFinal,
+	   &maxPath,
+	   &numLambdas,
+	   &lambdaMinRatio,
+	   userLambdasV,
+	   &convThreshold,
+	   &standardize,
+	   &maxit,
+	   &kopt,
+	   numFitsV,
+	   interceptsV,
+	   coeffsV,
+	   coeffPtrsV,
+	   coeffCntsV,
+	   dev0V,
+	   fdevV,
+	   lambdasUsedV,
+	   numPassesV,
+	   &error
+   );
+
+  fprintf(stderr, "Exiting splognet\n");
+
+  (*env)->ReleaseDoubleArrayElements(env, y, yv, JNI_ABORT);
+  (*env)->ReleaseDoubleArrayElements(env, offsets, ov, JNI_ABORT);
+  (*env)->ReleaseDoubleArrayElements(env, xx, xxv, JNI_ABORT);
+  (*env)->ReleaseIntArrayElements(env, xi, xiv, JNI_ABORT);
+  (*env)->ReleaseIntArrayElements(env, xp, xpv, JNI_ABORT);
+  (*env)->ReleaseIntArrayElements(env, mFlags, mFlagsV, JNI_ABORT);
+  (*env)->ReleaseDoubleArrayElements(env, penalties, penaltiesV, JNI_ABORT);
+  (*env)->ReleaseDoubleArrayElements(env, userLambdas, userLambdasV, JNI_ABORT);
+  (*env)->ReleaseIntArrayElements(env, outNumFits, numFitsV, 0);
+  (*env)->ReleaseDoubleArrayElements(env, outIntercepts, interceptsV, 0);
+  (*env)->ReleaseDoubleArrayElements(env, outCoeffs, coeffsV, 0);
+  (*env)->ReleaseIntArrayElements(env, outCoeffPtrs, coeffPtrsV, 0);
+  (*env)->ReleaseIntArrayElements(env, outCoeffCnts, coeffCntsV, 0);
+  (*env)->ReleaseDoubleArrayElements(env, outDev0, dev0V, 0);
+  (*env)->ReleaseDoubleArrayElements(env, outFdev, fdevV, 0);
   (*env)->ReleaseDoubleArrayElements(env, outLambdas, lambdasUsedV, 0);
   (*env)->ReleaseIntArrayElements(env, outNumPasses, numPassesV, 0);
   
--- src/java/glmnet/GLMNet.java
+++ src/java/glmnet/GLMNet.java
@@ -5,7 +5,10 @@ public class GLMNet {
 	System.loadLibrary("glmnet");
     }
 
-    public native double test(double[] d);
+    /*
+c call spelnet(ka,parm,no,ni,x,ix,jx,y,w,jd,vp,ne,nx,nlam,flmin,ulam,thr,
+c             isd,maxit,lmu,a0,ca,ia,nin,rsq,alm,nlp,jerr)
+    */
 
     public native int spelnet(
        int covUpdating,
@@ -31,6 +34,41 @@ public class GLMNet {
        int[] outCoeffPtrs, 
        int [] outCoeffCnts, 
        double[] outRsq, 
+       double[] outLambdas, 
+       int[] outNumPasses);
+
+    /*
+c call splognet (parm,no,ni,nc,x,ix,jx,y,o,jd,vp,ne,nx,nlam,flmin,
+c             ulam,thr,isd,maxit,kopt,lmu,a0,ca,ia,nin,dev0,fdev,alm,nlp,jerr)
+c
+    */
+
+    public native int splognet(
+       double alpha,
+       int nc,
+       double[] y,
+       double[] offsets,
+       double[] xx, 
+       int[] xi, 
+       int [] xp, 
+       int[] mFlags, 
+       double[] penalties, 
+       int maxFinal, 
+       int maxPath, 
+       int numLambdas, 
+       double lambdaMinRatio, 
+       double[] userLambdas, 
+       double convThreshold, 
+       int standardize, 
+       int maxit, 
+       int kopt,
+       int[] outNumFits, 
+       double[] outIntercepts, 
+       double[] outCoeffs, 
+       int[] outCoeffPtrs, 
+       int [] outCoeffCnts, 
+       double[] dev0,
+       double[] fdev,
        double[] outLambdas, 
        int[] outNumPasses);
 
